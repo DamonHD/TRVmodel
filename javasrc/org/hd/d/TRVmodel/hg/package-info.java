454b1cec51c65d7f5db3abe644e3f68ef2e1f83b
DamonHD <d@hd.org>
2023-06-14 13:35:17 +0100

    Created hg package.

---
+++ javasrc/org/hd/d/TRVmodel/hg/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Attempting to recreate and generalise the heat-pump / TRV / energy interactions from:
+ * <a href="https://www.heatgeek.com/why-not-to-zone-heat-pumps-or-boilers/">https://www.heatgeek.com/why-not-to-zone-heat-pumps-or-boilers/</a>
+ * as of ~2023-06.
+ */
+package org.hd.d.TRVmodel.hg;
