8c9b92adeccc3a56158138bc53597f172b48224b
DamonHD <d@hd.org>
2023-06-14 13:54:18 +0100

    WIP

--- javasrc/org/hd/d/TRVmodel/hg/package-info.java
+++ javasrc/org/hd/d/TRVmodel/hg/package-info.java
@@ -3,6 +3,9 @@
  * <a href="https://www.heatgeek.com/why-not-to-zone-heat-pumps-or-boilers/">https://www.heatgeek.com/why-not-to-zone-heat-pumps-or-boilers/</a>
  * as of ~2023-06.
  * <p>
- * This is examining if turning down TRVs in unused rooms saves energy or not.
+ * This is examining if turning down TRVs in unused rooms saves energy or not,
+ * as is commonly assumed eg with gas-fired heating.
+ * <p>
+ * This is a simple model, mirroring the page above.
  */
 package org.hd.d.TRVmodel.hg;
