04b2e8c5ddcbaf756f492f15918db8d02e4f9a2a
DamonHD <d@hd.org>
2023-06-14 14:47:35 +0100

    Description.

--- javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModel.java
+++ javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModel.java
@@ -9,6 +9,32 @@ package org.hd.d.TRVmodel.hg;
  * as is commonly assumed eg with gas-fired heating.
  * <p>
  * This is a simple model, mirroring the page above.
+ * <p>
+ * This model is steady-state, ie in temperature equilibrium.
+ * <p>
+ * In this model there are 4 rooms in house in a square in the pattern:
+ * <table border="1">
+ * <tr><td>A</td><td>B</td></tr>
+ * <tr><td>B</td><td>A</td></tr>
+ * </table>
+ * <p>
+ * Where the <em>A</em> rooms are kept at the 'normal' 21&deg;C living temperature,
+ * and the <em>B</em> rooms are kept at the same temperature as A or at a lower 18&deg;C setback.
+ * These temperatures (especially those in the B rooms) are regulated by TRVs
+ * controlling the radiator heat emitters in each room.
+ * <p>
+ * The rooms can be considered to be in a horizontal plane,
+ * with the area outside the rooms being at exterior temperature.
+ * <p>
+ * The external walls are of different construction to
+ * the internal walls between the A and B rooms.
+ * <p>
+ * Heat loss through ceilings and floors and all other routes is ignored.
+ * <p>
+ * This model looks to the total input electric energy input demand
+ * to a heat pump to maintain the specified steady state,
+ * taking into account implied flow water temperatures
+ * and the heat-pump CoP to reach those flow temperatures.
  */
 public final class HGTRVHPMModel
  	{
