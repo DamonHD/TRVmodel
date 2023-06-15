d10468f792f436aa7b608a91e266b97aac9a8252
DamonHD <d@hd.org>
2023-06-15 15:12:45 +0100

    HOME_HEAT_LOSS_DESIGN_TEMPERATURE

--- javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModel.java
+++ javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModel.java
@@ -52,11 +52,12 @@ public final class HGTRVHPMModel
     public static final double EXTERNAL_AIR_TEMPERATURE_C = -3;
     /**tInt - 'normal' room temperature (Celsius). */
     public static final double NORMAL_ROOM_TEMPERATURE_C = 21;
-    /**Design temperature (cold winter day) for heat loss calculations (Celsius). */
+    /**HLDT - design temperature (cold winter day) for heat loss calculations (Celsius). */
     public static final double HOME_HEAT_LOSS_DESIGN_TEMPERATURE =
 		(NORMAL_ROOM_TEMPERATURE_C - EXTERNAL_AIR_TEMPERATURE_C);
-    /**Heat loss with all rooms at normal internal temperature (W). */
+    /**HLW - eat loss with all rooms at normal internal temperature (W). */
     public static final double HOME_HEAT_LOSS_AT_NORMAL_ROOM_TEMPERATURE_W = 2000;
+
     /**tIntSetback - setback/unused room temperature (Celsius). */
     public static final double SETBACK_ROOM_TEMPERATURE_C = 18;
 
