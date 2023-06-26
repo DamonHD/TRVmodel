4b66db4799054b3f65f86484e2aa842368d893ee
DamonHD <d@hd.org>
2023-06-26 13:14:20 +0100

    WIP

--- javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelParameterised.java
+++ javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelParameterised.java
@@ -73,19 +73,71 @@ public final class HGTRVHPMModelParameterised
 
         // HEAT LOSS 1
     	// IWAabmd: (Heat Loss 1.2) internal wall area between each A and adjoining B rooms minus appropriate amount of door (m^2).
+    	// (INTERNAL_WALL_AREA_FROM_EACH_A_TO_B_ROOMS_MINUS_DOOR_M2)
     	final double IWAabmd =
 			HGTRVHPMModel.INTERNAL_WALL_AREA_FROM_EACH_A_TO_B_ROOMS_M2
 			- (2 * params.doorsPerInternalWall() * HGTRVHPMModel.INTERNAL_DOOR_AREA_PER_DOOR_M2);
     	// IWAabHL: (Heat Loss 1.3) internal wall (minus door) heat loss per Kelvin (W/K).
+    	// (INTERNAL_WALL_MINUS_DOOR_HEAT_LOSS_PER_KELVIN_WpK)
         final double IWAabHL =
     		IWAabmd * HGTRVHPMModel.INTERNAL_WALL_U_WpM2K;
         // IWAabHLW: (Heat Loss 1.4) internal wall (minus door) heat loss (WK).
+        // (INTERNAL_WALL_MINUS_DOOR_HEAT_LOSS_W)
         final double IWAabHLW =
     		IWAabHL *
     			(HGTRVHPMModel.NORMAL_ROOM_TEMPERATURE_C - HGTRVHPMModel.SETBACK_ROOM_TEMPERATURE_C);
         // IDAabHL: (Heat Loss 1.5) internal door heat loss per door per Kelvin (W/K).
+        // (INTERNAL_DOOR_HEAT_LOSS_PER_KELVIN_WpK)
         final double IDAabHL =
     		HGTRVHPMModel.INTERNAL_DOOR_AREA_PER_DOOR_M2 * HGTRVHPMModel.INTERNAL_DOOR_U_WpM2K;
+        // IDAabHLW: (Heat Loss 1.6) internal door heat loss per door (W).
+        // (INTERNAL_DOOR_HEAT_LOSS_W)
+        final double IDAabHLW =
+    		IDAabHL *
+    		    (HGTRVHPMModel.NORMAL_ROOM_TEMPERATURE_C - HGTRVHPMModel.SETBACK_ROOM_TEMPERATURE_C);
+
+
+//        /**IDWAabHLW: (Heat Loss 1.7) internal wall and door heat loss per A room (W). */
+//        public static final double INTERNAL_WALL_AND_DOOR_HEAT_LOSS_PER_A_ROOM_W =
+//        		INTERNAL_WALL_MINUS_DOOR_HEAT_LOSS_W + INTERNAL_DOOR_HEAT_LOSS_W;
+//
+//        /**radWAbs: (Heat Loss 2.0) radiator output in each A room when B setback (W). */
+//        public static final double RADIATOR_POWER_IN_A_ROOMS_WHEN_B_SETBACK_W =
+//    		RADIATOR_POWER_WITH_HOME_AT_NORMAL_ROOM_TEMPERATURE_W + INTERNAL_WALL_AND_DOOR_HEAT_LOSS_PER_A_ROOM_W;
+//        /**radWBbs: (Heat Loss 2.0) radiator output in each B room when B setback (W). */
+//        public static final double RADIATOR_POWER_IN_B_ROOMS_WHEN_B_SETBACK_W =
+//    		(HOME_HEAT_LOSS_B_SETBACK_W - 2*RADIATOR_POWER_IN_A_ROOMS_WHEN_B_SETBACK_W) / 2;
+//        /**radWAmult: (Heat Loss 2.1) radiator output increase multiplier in each A room when B setback. */
+//        public static final double RADIATOR_POWER_UPLIFT_IN_A_ROOMS_WHEN_B_SETBACK_MULTIPLIER =
+//    		RADIATOR_POWER_IN_A_ROOMS_WHEN_B_SETBACK_W / RADIATOR_POWER_WITH_HOME_AT_NORMAL_ROOM_TEMPERATURE_W;
+//     	/**MWATP2Dexp: (Heat Loss 2.2) exponent to go from power increase to delta-T increase. */
+//        public static final double RADIATOR_EXP_POWER_TO_DT = 0.77;
+//        /**radAbsdTmult: (Heat Loss 2.3) radiator MW-AT delta-T increase multiplier in each A room when B setback. */
+//        public static final double RADIATOR_DT_UPLIFT_IN_A_ROOMS_WHEN_B_SETBACK_MULTIPLIER =
+//    		Math.pow(RADIATOR_POWER_UPLIFT_IN_A_ROOMS_WHEN_B_SETBACK_MULTIPLIER, RADIATOR_EXP_POWER_TO_DT);
+//        /**radAbsdT: (Heat Loss 2.4) radiator MW-AT delta-T in each A room when B setback (K). */
+//        public static final double RADIATOR_DT_IN_A_ROOMS_WHEN_B_SETBACK_K =
+//    		RADIATOR_MWATDT_AT_NORMAL_ROOM_TEMPERATURE_W * RADIATOR_DT_UPLIFT_IN_A_ROOMS_WHEN_B_SETBACK_MULTIPLIER;
+//        /**radAbsMW: (Heat Loss 2.5) radiator mean water temperature in each A room when B setback (C). */
+//        public static final double RADIATOR_MW_IN_A_ROOMS_WHEN_B_SETBACK_C =
+//    		NORMAL_ROOM_TEMPERATURE_C + RADIATOR_DT_IN_A_ROOMS_WHEN_B_SETBACK_K;
+//
+//        /**CoPA2W46p0: (Heat Pump Efficiency) suggested HP CoP at 46.0C flow temperature. */
+//        public static final double COP_AT_46p0C = 2.6;
+//        /**CoPA2W51p5: (Heat Pump Efficiency) suggested HP CoP at 51.5C flow temperature. */
+//        public static final double COP_AT_51p5C = 2.3;
+//        /**CoPDropPerK: (Heat Pump Efficiency) suggested approximate fall in CoP per K rise in flow temperature. */
+//        public static final double COP_DROP_pK = 0.025;
+//        /**HPinWnsb: (Heat Pump Efficiency) heat-pump electrical power in when B not setback (W).
+//         * Note that flow and mean temperatures seem to be being mixed here.
+//         */
+//        public static final double HEAT_PUMP_POWER_IN_NO_SETBACK_W =
+//    		HOME_HEAT_LOSS_AT_NORMAL_ROOM_TEMPERATURE_W / COP_AT_46p0C;
+//        /**HPinWsb: (Heat Pump Efficiency) heat-pump electrical power in when B is setback (W).
+//         * Note that flow and mean temperatures seem to be being mixed here.
+//         */
+//        public static final double HEAT_PUMP_POWER_IN_B_SETBACK_W =
+//    		HOME_HEAT_LOSS_B_SETBACK_W / COP_AT_51p5C;
 
 
 
@@ -93,6 +145,9 @@ public final class HGTRVHPMModelParameterised
 
 
     	// TODO
+
+
+
 
 
 
