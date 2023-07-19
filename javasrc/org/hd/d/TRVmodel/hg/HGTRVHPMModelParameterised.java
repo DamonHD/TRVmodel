d570bb2c1abfd016abd0166f15998a4b555bf672
DamonHD <d@hd.org>
2023-07-19 11:04:22 +0100

    Added HeatAndElectricityDemand.

--- javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelParameterised.java
+++ javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelParameterised.java
@@ -87,6 +87,22 @@ public final class HGTRVHPMModelParameterised
     			DEFAULT_EXTERNAL_AIR_TEMPERATURE_C);
 	    }
 
+    /**Heat demand and the electricity input to the heat pump to meet it (W).
+     * @param heatDemand  raw heat demand to maintain home temperature; finite, non-negative
+     * @param heatPumpInput  electricity into the heat pump to maintain home temperature; finite, non-negative
+     */
+    public record HeatAndElectricityDemand(double heatDemand, double heatPumpInput)
+	    {
+	    public HeatAndElectricityDemand
+		    {
+    		// Sanity-check parameters.
+    		if(!Double.isFinite(heatDemand)) { throw new IllegalArgumentException(); }
+    		if(heatDemand < 0) { throw new IllegalArgumentException(); }
+    		if(!Double.isFinite(heatPumpInput)) { throw new IllegalArgumentException(); }
+    		if(heatPumpInput < 0) { throw new IllegalArgumentException(); }
+		    }
+	    }
+
     /**Estimate the CoP for a given flow temperature (C) given the two supplied data points.
      * This does a simple linear fit, which is not perfect but probably adequate.
      * <p>
