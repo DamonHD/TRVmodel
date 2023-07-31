37a608073632da0e9fb4f421f1e681c15f7f872d
DamonHD <d@hd.org>
2023-07-31 15:00:36 +0100

    Added ScenarioResult.

--- javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelByHour.java
+++ javasrc/org/hd/d/TRVmodel/hg/HGTRVHPMModelByHour.java
@@ -3,6 +3,7 @@ package org.hd.d.TRVmodel.hg;
 import java.util.Objects;
 
 import org.hd.d.TRVmodel.data.DDNTemperatureDataCSV;
+import org.hd.d.TRVmodel.hg.HGTRVHPMModelParameterised.DemandWithoutAndWithSetback;
 
 /**Drives the parameterised HG model variant(s) with hourly temperature data.
  */
@@ -16,4 +17,28 @@ public record HGTRVHPMModelByHour(HGTRVHPMModelParameterised model, DDNTemperatu
 
 	/**Temperature below which space heating is required, CIBSE typical UK threshold. */
 	public static final double DEFAULT_BASE_HEATING_TEMPERATURE_C = 15.5;
+
+	/**Results of running a model against a temperature set.
+	 * @param  fractionSetbackRaisesDemand  fraction of the hours in which setting back B rooms
+	 *     causes electricity demand by the heat pump to rise [0.0,1.0]
+	 * @param demand heat and electrical mean demand; not null
+	 *
+	 */
+	public record ScenarioResult(double fractionSetbackRaisesDemand, DemandWithoutAndWithSetback demand)
+		{
+		public ScenarioResult
+			{
+			if(!Double.isFinite(fractionSetbackRaisesDemand)) { throw new IllegalArgumentException(); }
+			if(fractionSetbackRaisesDemand < 0) { throw new IllegalArgumentException(); }
+			if(fractionSetbackRaisesDemand > 1) { throw new IllegalArgumentException(); }
+			Objects.requireNonNull(demand);
+			}
+		}
+
+	/**Run scenario on model and temperature; never null.
+	 */
+	public static ScenarioResult runScenario()
+		{
+throw new RuntimeException("NOT IMPLEMENTED");
+		}
  	}
