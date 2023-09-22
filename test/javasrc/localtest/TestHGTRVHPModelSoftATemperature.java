b3cf02025b588ce853ce5a35da0b3d0cecb09aa4
DamonHD <d@hd.org>
2023-09-22 14:14:31 +0100

    TestHGTRVHPModelSoftATemperature WIP.

--- test/javasrc/localtest/TestHGTRVHPModelSoftATemperature.java
+++ test/javasrc/localtest/TestHGTRVHPModelSoftATemperature.java
@@ -24,26 +24,22 @@ import junit.framework.TestCase;
 /**Test the parameterised Heat Geek TRV-with-HP model with soft A-room temperatures. */
 public final class TestHGTRVHPModelSoftATemperature extends TestCase
     {
-    /**Test with 'fix' parameters but otherwise original setup. */
+    /**Test bungalow with 'fix' parameters but otherwise original setup. */
     public static void testWithFixParameters()
 	    {
     	final HGTRVHPMModelParameterised.ModelParameters fixParams = HGTRVHPMModelParameterised.ModelParameters.FIXES_APPLIED;
     	final DemandWithoutAndWithSetback originalBungalowDemand = HGTRVHPMModelParameterised.computeBungalowDemandW(fixParams);
-//    	final DemandWithoutAndWithSetback detachedDemand = HGTRVHPMModelParameterised.computeDetachedDemandW(fixParams);
     	final DemandWithoutAndWithSetback softBungalowDemand = HGTRVHPMModelParameterised.computeSoftATempDemandW(fixParams, true);
-//    	final DemandWithoutAndWithSetback softDetachedDemand = HGTRVHPMModelParameterised.computeSoftATempDemandW(fixParams, true);
 
     	// Both heat demand and heat-pump electricity demand are expected to be identical to the original
     	// without B rooms set back.
     	assertEquals(originalBungalowDemand.noSetback().heatDemand(), softBungalowDemand.noSetback().heatDemand(), 1.0);
     	assertEquals(originalBungalowDemand.noSetback().heatPumpElectricity(), softBungalowDemand.noSetback().heatPumpElectricity(), 1.0);
 
-
-    	// Both heat demand and heat-pump electricity demand are expected to be strictly lower than the original
-    	// when B rooms are set back and A rooms have soft temperature regulation (weather compensation).
-//    	assertTrue(softBungalowDemand.
-
-
+//    	// Both heat demand and heat-pump electricity demand are expected to be strictly lower than the original
+//    	// when B rooms are set back and A rooms have soft temperature regulation (weather compensation).
+//    	assertTrue(originalBungalowDemand.noSetback().heatDemand() > softBungalowDemand.noSetback().heatDemand());
+//    	assertTrue(originalBungalowDemand.noSetback().heatPumpElectricity() > softBungalowDemand.noSetback().heatPumpElectricity());
 	    }
 
     }
