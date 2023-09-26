d269318bde258116933cf8a39adb3c9a2f5e6073
DamonHD <d@hd.org>
2023-09-26 16:08:58 +0100

    Tidy and new output captured.

--- javasrc/org/hd/d/TRVmodel/hg/ShowComputations.java
+++ javasrc/org/hd/d/TRVmodel/hg/ShowComputations.java
@@ -192,7 +192,7 @@ public final class ShowComputations
 
 
         System.out.println("");
-        System.out.println("Parameterised model, soft regulation, fixes applied for doors and CoP temperature, external air temperature varied...");
+        System.out.println("Parameterised model, bungalow, soft regulation, fixes applied for doors and CoP temperature, external air temperature varied...");
         System.out.println("London (EGLL) 2018 hourly temperatures");
 		for(final boolean abab : new boolean[]{true, false})
 			{
@@ -307,22 +307,4 @@ public final class ShowComputations
 		result.append("</table>");
 		return(result.toString());
 		}
-
-//	/**Generate the main full model results table for 7 places, 10 years, in (X)HTML5; non-null.
-//	 * Attempts to remain at least slightly human readable.
-//	 * <p>
-//	 * The table class at least should be fixed up manually.
-//	 */
-//	public static String generateHTMLMainFullTable() throws IOException
-//		{
-//		final StringBuilder result = new StringBuilder();
-//		result.append("<table class=\"yourHugeTableStyle\">\n");
-//
-//
-//		// TODO
-//
-//
-//		result.append("</table>");
-//		return(result.toString());
-//		}
 	}
